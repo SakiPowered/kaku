df4fd8c824525d33d470b95da3bac54dd9c2801e
FigT <figt@figt.us>
2024-02-08 19:07:09 -0800

    * Update LICENSE year

--- src/main/java/gg/saki/kaku/utils/StringUtil.java
+++ src/main/java/gg/saki/kaku/utils/StringUtil.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 SakiPowered
+ * Copyright (c) 2024 SakiPowered
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
